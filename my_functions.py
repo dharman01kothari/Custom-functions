7961aa7898cd7efda036aca959c88679e29d7225
dharman01kothari <65413671+dharman01kothari@users.noreply.github.com>
2020-05-28 19:56:03 -0400

    With User Input
    3/4 of way

--- my_functions.py
+++ my_functions.py
@@ -7,10 +7,18 @@ def celsius_to_fahrenheit(x):
  
 # TODO: define gradebook function here
 
-
-
-
-
+def numeric_to_letter_grade(n):
+    if n < 60:
+        return "F"
+    if n < 70:
+        return "D"
+    if n < 80:
+        return "C"
+    if n < 90:
+        return "B"
+    else:
+        return "A"
+        
 
 if __name__ == "__main__":
 
@@ -18,13 +26,13 @@ if __name__ == "__main__":
     print("CUSTOM FUNCTIONS EXERCISE...")
 
     print("--------------------")
-    c = 0
+    c = int(input('Please input a celcius temperature:'))
     print("THE CELSIUS TEMP IS:", c, "DEGREES")
     f = celsius_to_fahrenheit(c)
     print("THE FAHRENHEIT EQUIVALENT IS:", f, "DEGREES")
 
-    #print("--------------------")
-    #score = 87.5
-    #print("THE NUMERIC SCORE IS:", score)
-    #grade = numeric_to_letter_grade(score)
-    #print("THE LETTER-GRADE EQUIVALENT IS:", grade)
+    print("--------------------")
+    score = int(input('Please input a numerical score:'))
+    print("THE NUMERIC SCORE IS:", score)
+    grade = numeric_to_letter_grade(score)
+    print("THE LETTER-GRADE EQUIVALENT IS:", grade)
